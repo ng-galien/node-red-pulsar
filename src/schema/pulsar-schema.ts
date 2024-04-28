50d869232f08e88cdd1c1790b15b4dc7098c2423
Alexandre Boyer <33391039+ng-galien@users.noreply.github.com>
2024-04-28 23:02:09 +0200

    Refactor createSchemaConfig method and add documentation
    The createSchemaConfig method in pulsar-schema.ts has been refactored with a new name - createSchemaInfo. Detailed comments have also been added to explain both the createSchemaInfo function and the main module export, providing a better understanding of the method purposes and parameters.

--- src/schema/pulsar-schema.ts
+++ src/schema/pulsar-schema.ts
@@ -2,21 +2,41 @@ import * as NodeRED from 'node-red'
 import {parseMandatoryEnum, PulsarSchemaConfig, PulsarSchemaId} from "../PulsarDefinition";
 import {SchemaInfo, SchemaType} from "pulsar-client";
 
-function createSchemaConfig(config: PulsarSchemaConfig): SchemaInfo {
+
+/**
+ * Creates a schema information object from the provided configuration.
+ *
+ * @param {PulsarSchemaConfig} config - The configuration object containing the schema information.
+ * @property {string} config.name - The name of the schema.
+ * @property {string} config.type - The type of the schema.
+ * @property {*} config.schema - The schema object.
+ *
+ * @return {SchemaInfo} - The created schema configuration object.
+ * @property {string} SchemaInfo.name - The name of the schema.
+ * @property {SchemaType} SchemaInfo.schemaType - The type of the schema.
+ * @property {*} SchemaInfo.schema - The schema object.
+ * @property {undefined} SchemaInfo.properties - The properties of the schema (currently undefined).
+ */
+function createSchemaInfo(config: PulsarSchemaConfig): SchemaInfo {
     return {
         name: config.name,
         schemaType: parseMandatoryEnum<SchemaType>(config.type),
         schema: config.schema,
         properties: undefined
     }
-
 }
 
+/**
+ * Registers the Pulsar Schema node with the provided RED instance.
+ *
+ * @param {NodeRED.NodeAPI} RED - The RED instance to register the node with.
+ * @return {void} - No return value.
+ */
 export = (RED: NodeRED.NodeAPI): void => {
     RED.nodes.registerType(PulsarSchemaId,
         function (this: NodeRED.Node<SchemaInfo>, config: PulsarSchemaConfig): void {
             RED.nodes.createNode(this, config)
-            this.credentials = createSchemaConfig(config)
+            this.credentials = createSchemaInfo(config)
         }
     )
 }
