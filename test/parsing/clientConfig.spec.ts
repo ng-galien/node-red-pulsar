ae31f760509c99f086d74bf845c1ba75df03d3c9
Alexandre Boyer <33391039+ng-galien@users.noreply.github.com>
2025-02-15 22:53:56 +0100

    Add unit tests for validatePulsarUrl function to ensure proper URL validation

--- test/parsing/clientConfig.spec.ts
+++ test/parsing/clientConfig.spec.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { PulsarClientConfig } from '../../src/PulsarDefinition';
 import { ClientConfig } from 'pulsar-client';
-import { clientConfig } from '../../src/PulsarConfig';
+import { clientConfig, validatePulsarUrl } from '../../src/PulsarConfig';
 import { Node } from 'node-red';
 
 describe('clientConfig', () => {
@@ -26,5 +26,13 @@ describe('clientConfig', () => {
     );
     expect(config).to.be.an('object');
     expect(config.serviceUrl).to.equal('pulsar://localhost:6650');
+  });
+
+  it('pulsar url should be valid', () => {
+    expect(validatePulsarUrl('pulsar://localhost:6650')).to.equal(true);
+    expect(validatePulsarUrl('pulsar+ssl://localhost:6651')).to.equal(true);
+    expect(validatePulsarUrl('')).to.equal(false);
+    expect(validatePulsarUrl('this is not a valid url')).to.equal(false);
+    expect(validatePulsarUrl('http://localhost:8080')).to.equal(false);
   });
 });
